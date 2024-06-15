8e75576a85aeba6a71eba820e0880e62e32d1d61
Shreya Singh <singhsshreya297@gmail.com>
2024-06-15 16:33:04 +0530

    first commit

---
+++ src/Component/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar=()=>{
+    return(
+    <div className="navbar">
+     <a href="#!">F<span>oo</span>diesHub</a> 
+     <div className="nav-links">
+         <a href="#!">Home</a>
+         <a href="#!">Receipes</a>
+         <a href="#!">Settings</a>
+
+     </div>
+    </div>
+    )
+}
+export default Navbar;
