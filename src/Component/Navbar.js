b725a4254f252bf1bc5c7c6f0c01d4d94b2d2d5c
Shreya Singh <singhsshreya297@gmail.com>
2024-06-15 22:48:22 +0530

    Changes

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,4 +1,7 @@
+import {useState} from "react";
+
 const Navbar=()=>{
+    const [showSidebar,setshowSidebar]=useState(false);
     return(
     <div className="navbar container">
      <a href="#!" className="logo">F<span>oo</span>diesHub</a> 
@@ -8,6 +11,12 @@ const Navbar=()=>{
          <a href="#!">Settings</a>
 
      </div>
+    {/* hang burger item */}
+    <div onClick={()=> setshowSidebar(!showSidebar)}className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+    <div className="bar"></div>
+    <div className="bar"></div>
+    <div className="bar"></div>
+    </div>
     </div>
     )
 }
